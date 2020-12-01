05e1e400c9d25d57ffc9d205e6c42623b6e72eb2
Vitali Akulau <jerimi2008@tut.by>
2020-12-01 12:45:39 +0300

    Automate test case for New Customer suite

---
+++ cypress/page_object/NewCustomerPage.js
@@ -0,0 +1,32 @@
+class NewCutomerPage {
+  // Warning notification
+  warningNotification = 'div#fieldIncorrect';
+  
+  // Fields for new customer registration
+  emailAddressInput = 'input#email.textInput';
+  newPasswordInput = 'input#newPassword.textInput';
+  confirmNewPasswordInput = 'input#reenterPassword.textInput';
+  firstNameInput = 'input#givenName.textInput';
+  lastNameInput = 'input#surname.textInput';
+  institutionInput = '#JBJSInstitution.text-box';
+  professionLabelDDL = 'select#ProfessionId';
+  subspecialtyDDL = 'select#SubSpecialtyId';
+  secondarySubspecialtyDDL = 'select#SecondarySubSpecialtyId';
+  yearsOfExperienceDDL = 'select#YearOfExperience';
+
+  // Create button
+  createButton = 'button#continueb';
+
+  typeText(element, text){
+    cy.get(element)
+      .clear()
+      .type(text);
+    return this;
+  };
+
+  selectOption(element, option){
+    cy.get(element).select(option);
+    return this;
+  };
+};
+export default new NewCutomerPage();
