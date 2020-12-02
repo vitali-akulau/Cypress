9e59ce96e657a95dc2f01a18d9c732a858429a9f
Vitali Akulau <jerimi2008@tut.by>
2020-12-02 11:57:33 +0300

    Automate test cases for Sign in suite

--- cypress/page_object/SignInPage.js
+++ cypress/page_object/SignInPage.js
@@ -1,4 +1,15 @@
 class SignInPage {
   registerButton = 'button#register';
+  warningNotification = '.error.pageLevel p';
+  emailAddressInput = '#logonIdentifier';
+  passwordInput = '#password';
+  signInButton = '.buttons #next';
+
+  typeText(element, text){
+    cy.get(element)
+      .clear()
+      .type(text);
+    return this;
+  };
 };
 export default new SignInPage();
